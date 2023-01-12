bf4ba4386458791663ed8eb40f26b12d44b45eb5
russgo1 <russellg580@gmail.com>
2023-01-12 11:31:26 -0500

    Through 14.4.2

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,7 +4,7 @@
 console.log("logic working");
 
 // create map object
-let map = L.map('mapid').setView([34.0522, -118.2437], 14); // 14 is zoom level (scale 0-18)
+let map = L.map('mapid').setView([34.0522, -118.2437], 4); // 14 is zoom level (scale 0-18)
 // Alternative:
 // This method is useful when we need to add multiple tile layers, or a background image of our map(s), which we will do later in this module.
 //let map = L.map("mapid", {
@@ -14,26 +14,48 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14); // 14 is zoom level 
     //zoom: 4
   //});
 
-// create tile layer
+
+
+
 
 // 14.4.1
 // Add marker
+/*
 var marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-/*
+
 L.circle([34.0522, -118.2437], {
     radius: 300,
     fillColor: "yellow",
     color: "black",
     fillOpacity: 0.25
 }).addTo(map);
-*/
+
 L.circleMarker([34.0522, -118.2437], {
     radius: 300,
     color: "black",
     fillColor: "#ffffa1"
 }).addTo(map);
+*/
 
+// 14.4.2
+// Get data from cities.js
+let cityData = cities;
+
+// Loop thorugh citites to create one marker for each
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: ((city.population - 200000) / 100000),
+    lineWeight: 4,
+    color: "orange",
+    fillColor: "orange"
+  })
+  .bindPopup(`<h2>${city.city}, ${city.state}</h2> <hr> <h3>Population ${city.population.toLocaleString()}</h3>`)
+  .addTo(map)
+});
+
+// create tile layer
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
