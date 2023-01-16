e8e6a8564271d0f7f02d4c12b4c3dad82a3b866c
russgo1 <russellg580@gmail.com>
2023-01-16 13:23:19 -0500

    add conditonal colors and popups under style function

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -37,20 +37,59 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
             return L.circleMarker(latlng);
         },
     // function to stylize L.geoJSON
-    style: styleInfo
+    style: styleInfo,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup(`Magnitude: ${feature.properties.mag}<br>Location: ${feature.properties.place}`);
+        }
     }).addTo(map);
     // function to define parameters of style function - returns object
     function styleInfo(feature) {
         return {
             opacity: 1,
             fillOpacity: 1,
-            fillColor: "#ffae42",
+            fillColor: getColor(feature.properties.mag),
             color: "#000000",
             // pass this part of the data object to the function
             radius: getRadius(feature.properties.mag),
             stroke: true,
             weight: 0.5
         };
+    };
+    // function to determine marker color based on magnitude
+    function getColor(magnitude) {
+        /*
+        switch (magnitude) {
+        case (magnitude > 5):
+            return "#ea2c2c";
+        case (magnitude > 4):
+            return "#ea822c";
+        case (magnitude > 3):
+            return "#ee9c00";
+        case (magnitude > 2):
+            return "#eecc00";
+        case (magnitude > 1):
+            return "#d4ee00";
+        case (magnitude < 1):
+            return "#98ee00";
+        }
+    };
+    */
+        if (magnitude > 5) {
+            return "#ea2c2c";
+        }
+        if (magnitude > 4) {
+            return "#ea822c";
+        }
+        if (magnitude > 3) {
+            return "#ee9c00";
+        }
+        if (magnitude > 2) {
+            return "#eecc00";
+        }
+        if (magnitude > 1) {
+            return "#d4ee00";
+        }
+          return "#98ee00";
     };
     // function to define radius value in styleInfo function
     function getRadius(magnitude) {
